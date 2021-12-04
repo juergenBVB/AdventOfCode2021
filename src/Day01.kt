cea0c35f70efd025272c6fb90502ebd0fd13edf2
Jürgen Huber <jue.huber@gmail.com>
2021-12-04 13:20:36 +0100

    day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val intList = input.map { value -> value.toInt() }
+        var increaseCounter = 0;
+        for (i in 1 until intList.size) {
+            if (intList[i] > intList[i-1]) {
+                increaseCounter += 1;
+            }
+        }
+
+        return increaseCounter;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val intList = input.map { value -> value.toInt() }
+        var increaseCounter = 0;
+        for (i in intList.indices) {
+            if (i + 4 > intList.size) {
+                break
+            }
+            
+            val firstWindow = intList[i] + intList[i + 1] + intList[i + 2]
+            val secondWindow = intList[i + 1] + intList[i + 2] + intList[i + 3]
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+            if (secondWindow > firstWindow) {
+                increaseCounter += 1;
+            }
+        }
+
+        return increaseCounter;
+    }
 
     val input = readInput("Day01")
     println(part1(input))
